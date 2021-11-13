24b12c7995cd27d7e559e78be92ead98aa60696e
DonPangPang <powertle@outlook.com>
2021-11-13 23:59:15 +0800

    添加项目文件。

---
+++ src/Pang.IoTPlatform.Core/MessageQueueProtocol/DownstreamProtocol.cs
@@ -0,0 +1,9 @@
+namespace Pang.IoTPlatform.Core.MessageQueueProtocol
+{
+    /// <summary>
+    /// 下行通信协议
+    /// </summary>
+    public class DownstreamProtocol
+    {
+    }
+}
---
+++ src/Pang.IoTPlatform.Core/MessageQueueProtocol/UpstreamProtocol.cs
@@ -0,0 +1,71 @@
+using System;
+using System.ComponentModel.Design.Serialization;
+using System.Runtime.CompilerServices;
+
+namespace Pang.IoTPlatform.Core.MessageQueueProtocol
+{
+    /// <summary>
+    /// 上行通信协议
+    /// </summary>
+    internal interface IUpstreamProtocol<out T>
+    {
+        string Serialize();
+
+        T Deserialize();
+    }
+
+    /// <summary>
+    /// 上行通信协议
+    /// </summary>
+    /// <remarks> 协议体为流 </remarks>
+    public class UpstreamProtocolStream : IUpstreamProtocol<UpstreamProtocolStream>
+    {
+        /// <summary>
+        /// 协议头, 用于标记协议类型
+        /// </summary>
+        public short Protocol { get; set; } = default!;
+
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <returns> </returns>
+        public string Serialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        /// <returns> </returns>
+        public UpstreamProtocolStream Deserialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// 上行通信协议
+    /// </summary>
+    /// <remarks> 协议体为字符串 </remarks>
+    public class UpstreamProtocolString : IUpstreamProtocol<UpstreamProtocolString>
+    {
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <returns> </returns>
+        public string Serialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        /// <returns> </returns>
+        public UpstreamProtocolString Deserialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
